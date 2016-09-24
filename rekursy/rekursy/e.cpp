91f2b350fb9509b63df9607c2c8c777d7d5642cf
smptsh <theolgagrig@gmail.com>
2016-09-24 10:09:35 +0800

    j

---
+++ rekursy/rekursy/e.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int f(int a)
+{
+	if (a==1)
+		return 1;
+	else return f(a-1)*a;
+}
+void main()
+{
+	int c=f(6);
+	cout << c <<endl;
+	system("pause");
+}
+
